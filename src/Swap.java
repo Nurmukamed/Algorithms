a561faa6431341c9189d1d2061437a1e592569d1
Nurmukamed <nikonur9403gmail.com>
2021-11-21 15:08:58 +0600

    practice

---
+++ src/Swap.java
@@ -0,0 +1,15 @@
+public class Swap {
+    public static void main(String[] args) {
+        swap(1, 3);
+        swap(9, 5);
+    }
+
+    public static void swap(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+//        a = a + b - (b = a);
+        System.out.println(a + " : " + b);
+    }
+}
